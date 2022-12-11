6b6f9ce78dab7bad4a809063eb95d80cc5f8478e
JenniferVeismann <117178110+jenniveismann@users.noreply.github.com>
2022-12-11 15:35:05 +0200

    Update Contact.vue
    aadress

--- HW/HW-VUE/src/components/Contact.vue
+++ HW/HW-VUE/src/components/Contact.vue
@@ -5,7 +5,7 @@
       <p>Feel free to contact us regardless of the question, we will answer you as soon as we can. </p>
       <strong>Our gmail is: </strong><label for="gmail">  Siim.Saaresalu@gmail.com</label><br><br>
       <strong>Our phone number is: </strong><label for="Phone"> +372 53038010</label><br><br>
-      <strong>Our address is:</strong><label for="where"> You can find us at Narva mnt 20</label>
+      <strong>Our address is:</strong><label for="where"> You can find us at Lutsu 12, Tartu</label>
     </div>
   </div>
 
